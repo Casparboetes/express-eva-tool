b7ceab7d466582fa17f4dd015163c8ac0f9b09f0
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-01-27 12:44:16 +0100

    Add Cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const passport = require('./config/auth')
 const { students, batches, users, sessions } = require('./routes')
@@ -9,15 +10,16 @@ let app = express()
 
 
 app
+  .use(cors())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(passport.initialize())
-  .use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
-  next()
-  })
+  // .use(function(req, res, next) {
+  // res.header('Access-Control-Allow-Origin', '*')
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  // res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+  // next()
+  // })
   .use(batches)
   .use(students)
   .use(users)
--- routes/batches.js
+++ routes/batches.js
@@ -16,7 +16,7 @@ router.get('/batches', (req, res, next) => {
       })
       .catch((error) => next(error))
   })
-  .post('/batches', (req, res, next) => {
+  .post('/batches', authenticate, (req, res, next) => {
     let newBatch = req.body
 
     Batch.create(newBatch)
@@ -24,39 +24,41 @@ router.get('/batches', (req, res, next) => {
       .catch((error) => next(error))
   })
 
-  .put('/batches/:id', (req, res, next) => {
-    const batchId = req.params.id
-    let update = req.body
+  // .put('/batches/:id', (req, res, next) => {
+  //   const batchId = req.params.id
+  //   let update = req.body
+  //
+  //   Batch.findOneAndUpdate(batchId,update)
+  //     .then((batch) => {
+  //       if (!batch) { return next() }
+  //       res.json(batch)
+  //     })
+  //     .catch((error) => next(error))
+  // })
 
-    Batch.findOneAndUpdate(batchId,update)
-      .then((batch) => {
-        if (!batch) { return next() }
-        res.json(batch)
-      })
-      .catch((error) => next(error))
-  })
+  // .patch('/batches/:id', (req, res, next) => {
+  //   const batchId = req.params.id
+  //   let update = req.body
+  //
+  //   Batch.findOneAndUpdate(batchId,update)
+  //     .then((batch) => {
+  //       if (!batch) { return next() }
+  //       res.json(batch)
+  //     })
+  //     .catch((error) => next(error))
+  // })
 
-  .patch('/batches/:id', (req, res, next) => {
-    const batchId = req.params.id
-    let update = req.body
+  // .delete('/batches/:id', (req, res, next) => {
+  //   const batchId = req.params.id
+  //
+  //   Batch.findOneAndRemove(batchId)
+  //     .then((batch) => {
+  //       if (!batch) { return next() }
+  //       res.json(batch)
+  //     })
+  //     .catch((error) => next(error))
+  // })
 
-    Batch.findOneAndUpdate(batchId,update)
-      .then((batch) => {
-        if (!batch) { return next() }
-        res.json(batch)
-      })
-      .catch((error) => next(error))
-  })
-
-  .delete('/batches/:id', (req, res, next) => {
-    const batchId = req.params.id
-
-    Batch.findOneAndRemove(batchId)
-      .then((batch) => {
-        if (!batch) { return next() }
-        res.json(batch)
-      })
-      .catch((error) => next(error))
-  })
+// router.use('/batches/:id/students', students)
 
 module.exports = router
